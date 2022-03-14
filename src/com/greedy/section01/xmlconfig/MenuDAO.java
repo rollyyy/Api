cdfea772c6542c29a84f16c1ed413ef6caafc665
yoonjeong Kim <kim785392@gmail.com>
2022-03-14 22:11:55 +0900

    commit

---
+++ src/com/greedy/section01/xmlconfig/MenuDAO.java
@@ -0,0 +1,29 @@
+package com.greedy.section01.xmlconfig;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class MenuDAO {
+
+	public List<MenuDTO> selectAllMenu(SqlSession sqlSession) {
+		return sqlSession.selectList("MenuMapper.selectAllMenu");
+	}
+
+	public MenuDTO selectMenuByCode(SqlSession sqlSession, int code) {
+		return sqlSession.selectOne("MenuMapper.selectMenuByCode", code);
+	}
+
+	public int insertMenu(SqlSession sqlSession, MenuDTO menu) {
+		return sqlSession.insert("MenuMapper.insertMenu", menu);
+	}
+
+	public int updateMenu(SqlSession sqlSession, MenuDTO menu) {
+		return sqlSession.update("MenuMapper.updateMenu", menu);
+	}
+
+	public int deleteMenu(SqlSession sqlSession, int code) {
+		return sqlSession.delete("MenuMapper.deleteMenu", code);
+	}
+
+}
